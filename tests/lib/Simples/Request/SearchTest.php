b61db4b7c6d548965264df020683fc010bd61582
scharrier <scharrier@gmail.com>
2013-05-22 15:51:43 +0200

    Added a sleep(1) to correct unit tests with "no active shards" errors.

--- tests/lib/Simples/Request/SearchTest.php
+++ tests/lib/Simples/Request/SearchTest.php
@@ -3,44 +3,46 @@
 require_once(dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'bootstrap.php');
 
 class Simples_Request_SearchTest extends PHPUnit_Framework_TestCase {
-	
+
 	public $client ;
-	
+
 	public function setUp() {
 		$this->client = new Simples_Transport_Http(array(
 			'index' => 'twitter',
 			'type' => 'tweet'
 		));
-		
+
 		$this->client->deleteIndex()->execute() ;
-		
+
 		$res = $this->client->index(array(
 			'content' => 'First',
 			'user' => 'scharrier',
 			'type' => 'tweet',
 			'priority' => 1
 		), array('id' => 1, 'refresh' => true))->execute();
-		
+
 		$res = $this->client->index(array(
 			'content' => 'Second',
 			'retweet' => 'scharrier',
 			'type' => 'retweet',
 			'priority' => 2
 		), array('id' => 2, 'refresh' => true))->execute();
-		
+
 		$res = $this->client->index(array(
 			'content' => 'First',
 			'user' => 'vtechnologies',
 			'type' => 'tweet',
 			'priority' => 2
 		), array('id' => 3, 'refresh' => true))->execute();
+
+		sleep(1) ;
 	}
 
 	public function testSearch() {
 		$request = $this->client->search() ;
 		$request->match('scharrier')->size(0)->explain(false) ;
 		$res = $request->to('array') ;
-		
+
 		// Base search tests
 		$expected = array(
 			'query' => array(
@@ -52,19 +54,19 @@ class Simples_Request_SearchTest extends PHPUnit_Framework_TestCase {
                         'explain' => false
 		) ;
 		$this->assertEquals($expected, $res) ;
-		
-		$res = $request->execute() ;		
+
+		$res = $request->execute() ;
 		$this->assertEquals(2, $res->hits->total) ;
-		
+
 		$this->assertEquals(1, $this->client->search('retweet:scharrier')->hits->total) ;
-		
+
 		$request = $this->client->search(array(
 			'query' => array(
 				'term' => array('user' => 'scharrier')
 			)
 		));
 		$this->assertEquals(1, $request->hits->total) ;
-		
+
 		// Base facet tests
 		$request = $this->client->search(array(
 			'facets' => array(
@@ -76,7 +78,7 @@ class Simples_Request_SearchTest extends PHPUnit_Framework_TestCase {
 		$facets = $response->facets->type->terms->to('array') ;
 		$this->assertEquals(2, $facets[0]['count']) ;
 		$this->assertEquals(1, $facets[1]['count']) ;
-		
+
 	}
 
 	public function test_sort() {
@@ -93,13 +95,13 @@ class Simples_Request_SearchTest extends PHPUnit_Framework_TestCase {
 		$body = $request->body() ;
 		$this->assertEquals(array('Client.name' => 'asc', 'Client.age' => 'desc', 'Client.location'), $body['sort']) ;
 	}
-	
+
 	public function testFluid() {
 		$request = $this->client->search();
 		$request->query('scharrier')
 				->from(10)
 				->size(5) ;
-		
+
 		$body = $request->body() ;
 		$this->assertEquals('query', key($body)) ;
 		$this->assertEquals('scharrier', $body['query']['query_string']['query']) ;
@@ -129,7 +131,7 @@ class Simples_Request_SearchTest extends PHPUnit_Framework_TestCase {
 		$this->assertTrue($filter2 instanceof Simples_Request_Search_Criteria_Filter) ;
 		$this->assertNotEquals($filter->to('array'), $filter2->to('array')) ;
 	}
-	
+
 	public function testQueryBuilder() {
 		// Base case
 		$request = $this->client->search()->query('scharrier')->in('username') ;
@@ -143,7 +145,7 @@ class Simples_Request_SearchTest extends PHPUnit_Framework_TestCase {
 		) ;
 		$this->assertTrue($request instanceof Simples_Request) ;
 		$this->assertEquals($expected, $res) ;
-		
+
 		// Complex
 		$request = $this->client->search()
 			->query()
@@ -153,19 +155,19 @@ class Simples_Request_SearchTest extends PHPUnit_Framework_TestCase {
 				->not()
 					->field('connected')->match(true)
 			->size(5) ;
-		
+
 		$res = $request->to('array') ;
 		$this->assertTrue(isset($res['query']['bool']['should'])) ;
 		$this->assertEquals(2, count($res['query']['bool']['should'])) ;
 		$this->assertTrue(isset($res['query']['bool']['must_not'])) ;
 		$this->assertEquals(1, count($res['query']['bool']['must_not'])) ;
 		$this->assertEquals(5, $res['size']) ;
-		
+
 		// Real call check
 		$response = $request->execute() ;
 		$this->assertTrue(isset($response->took)) ;
 	}
-	
+
 	public function testFilterBuilder() {
 		// Base case
 		$request = $this->client->search()->filter('scharrier')->in('username') ;
@@ -207,7 +209,7 @@ class Simples_Request_SearchTest extends PHPUnit_Framework_TestCase {
 		) ;
 		$this->assertEquals($expected, $res) ;
 	}
-        
+
     public function testFacetsBuilder() {
 		// Base case
 		$request = $this->client->search()->facet('username') ;
@@ -254,7 +256,7 @@ class Simples_Request_SearchTest extends PHPUnit_Framework_TestCase {
 		);
 		$this->assertEquals($expected, $res) ;
 	}
-	
+
 	public function testMultipleQueries() {
 		$request = $this->client->search()->queries(array(
 			'firstname' => 'Sebastien',
@@ -264,7 +266,7 @@ class Simples_Request_SearchTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('Sebastien', $res['query']['bool']['must'][0]['term']['firstname']) ;
 		$this->assertEquals(array('Charrier','Morrison'), $res['query']['bool']['must'][1]['terms']['lastname']) ;
 	}
-	
+
 	public function testMultipleFilters() {
 		$request = $this->client->search()->filters(array(
 			'firstname' => 'Sebastien',
@@ -274,7 +276,7 @@ class Simples_Request_SearchTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('Sebastien', $res['filter']['bool']['must'][0]['term']['firstname']) ;
 		$this->assertEquals(array('Charrier','Morrison'), $res['filter']['bool']['must'][1]['terms']['lastname']) ;
 	}
-	
+
 	public function testMultipleFacets() {
 		$request = $this->client->search()->facets(array(
 			'name' => 'firstname',
@@ -284,7 +286,7 @@ class Simples_Request_SearchTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('firstname', $res['facets']['name']['terms']['field']) ;
 		$this->assertEquals(array('firstname','lastname'), $res['facets']['full']['terms']['fields']) ;
 	}
-	
+
 	public function testFullRequest() {
 		$request = $this->client->search()
 			->query()
@@ -295,14 +297,14 @@ class Simples_Request_SearchTest extends PHPUnit_Framework_TestCase {
 			->facets(array('type','level'))
 			->size(2) ;
 		$res = $request->to('array') ;
-		
+
 		$this->assertEquals('Sebastien', $res['query']['query_string']['query']) ;
 		$this->assertEquals('administrator', $res['filter']['bool']['must'][0]['term']['type']) ;
 		$this->assertEquals(1, $res['filter']['bool']['must'][1]['term']['level']) ;
 		$this->assertEquals(2, count($res['facets'])) ;
 		$this->assertEquals(2, $res['size']) ;
 	}
-	
+
 	public function testHighlight() {
 		$request = $this->client->search()->highlight(array(
 			'fields' => array(
@@ -316,7 +318,7 @@ class Simples_Request_SearchTest extends PHPUnit_Framework_TestCase {
 		$this->assertTrue(isset($res['highlight']['fields']['name'])) ;
 		$this->assertTrue(isset($res['highlight']['fields']['address'])) ;
 	}
-	
+
 	public function testOptions() {
 		$request = $this->client->search() ;
 		$request->add(array('query' => 'sebastien','in' => 'name'), array('type' => 'text')) ;
