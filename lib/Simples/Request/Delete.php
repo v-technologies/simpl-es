4427c21080626965149bc6c432afe2e68ad76e64
scharrier <scharrier@gmail.com>
2013-10-11 16:15:50 +0200

    Authorize use of "refresh" param. Migrates the path generation on the Simples_Path mode.

--- lib/Simples/Request/Delete.php
+++ lib/Simples/Request/Delete.php
@@ -2,40 +2,53 @@
 
 /**
  * Delete and object.
- * 
+ *
  * @author Sébastien Charrier <scharrier@gmail.com>
- * @package	Simples
+ * @package    Simples
  * @subpackage Request
  */
 class Simples_Request_Delete extends Simples_Request {
-	
-	/**
-	 * Definition
-	 * 
-	 * @var array
-	 */
-	protected $_definition = array(
-		'method' => self::DELETE,
-		'required' => array(
-			'body' => array('id'),
-			'options' => array('index','type')
-		),
-		'magic' => 'id'
-	) ;
-	
-	/**
-	 * Path : id management.
-	 * 
-	 * @return string	API path
-	 */
-	public function path() {
-		$path = parent::path() ;
-		
-		// Object id transmited : we had it to the url.
-		if (isset($this->_body['id'])) {
-			$path .= $this->_body['id'] . '/' ;
-		}
-		
-		return $path ;
-	}
+
+    /**
+     * Definition
+     *
+     * @var array
+     */
+    protected $_definition = array(
+        'method' => self::DELETE,
+        'required' => array(
+            'body' => array('id'),
+            'options' => array('index','type')
+        ),
+        'magic' => 'id',
+        'inject' => array(
+            'params' => array('refresh'),
+        )
+    ) ;
+
+    /**
+     * Request options :
+     * - refresh (bool) : should we wait the index refresh before continuing ?
+     *
+     * @var array
+     */
+    protected $_options = array(
+        'refresh' => null,
+    ) ;
+
+    /**
+     * Path : id management.
+     *
+     * @return string    API path
+     */
+    public function path() {
+        $path = parent::path() ;
+
+        // Object id transmited : we add it to the url.
+        if (isset($this->_body['id'])) {
+            $path->directory($this->_body['id']) ;
+        }
+
+        return $path ;
+    }
 }
