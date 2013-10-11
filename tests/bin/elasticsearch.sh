2f562969361f7149a1cbd3684d3a7eb32c8c9b82
scharrier <scharrier@gmail.com>
2013-10-11 15:33:31 +0200

    Elasticsearch 0.90.5 compatibility tests.

--- tests/bin/elasticsearch.sh
+++ tests/bin/elasticsearch.sh
@@ -2,18 +2,18 @@
 
 # From Elastica unit tests. https://github.com/ruflin/Elastica/tree/master/test/bin
 
-wget http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.20.5.tar.gz
-tar -xzf elasticsearch-0.20.5.tar.gz
-sed 's/# index.number_of_shards: 1/index.number_of_shards: 1/' elasticsearch-0.20.5/config/elasticsearch.yml > elasticsearch-0.20.5/config/elasticsearch.yml
-sed 's/# index.number_of_replicas: 0/index.number_of_replicas: 0/' elasticsearch-0.20.5/config/elasticsearch.yml > elasticsearch-0.20.5/config/elasticsearch.yml
-sed 's/# discovery.zen.ping.multicast.enabled: false/discovery.zen.ping.multicast.enabled: false/' elasticsearch-0.20.5/config/elasticsearch.yml > elasticsearch-0.20.5/config/elasticsearch.yml
+wget http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.5.tar.gz
+tar -xzf elasticsearch-0.90.5.tar.gz
+sed 's/# index.number_of_shards: 1/index.number_of_shards: 1/' elasticsearch-0.90.5/config/elasticsearch.yml > elasticsearch-0.90.5/config/elasticsearch.yml
+sed 's/# index.number_of_replicas: 0/index.number_of_replicas: 0/' elasticsearch-0.90.5/config/elasticsearch.yml > elasticsearch-0.90.5/config/elasticsearch.yml
+sed 's/# discovery.zen.ping.multicast.enabled: false/discovery.zen.ping.multicast.enabled: false/' elasticsearch-0.90.5/config/elasticsearch.yml > elasticsearch-0.90.5/config/elasticsearch.yml
 
 export JAVA_OPTS="-server"
-elasticsearch-0.20.5/bin/elasticsearch &
+elasticsearch-0.90.5/bin/elasticsearch &
 
 echo "Waiting until elasticsearch is ready on port 9200"
 while [[ -z `curl -s 'http://localhost:9200' ` ]]
-do 
+do
 	echo -n "."
 	sleep 2s
 done
